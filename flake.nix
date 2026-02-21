0a510cb32573987d66b9e749637d2136ba9c29ef
Stein Martin Hustad <stein@hustad.com>
2026-02-21 09:20:07 +0400

    fix: add autoPatchelfHook for NixOS ELF interpreter patching

--- flake.nix
+++ flake.nix
@@ -32,6 +32,13 @@
           inherit version;
           src = pkgs.fetchurl { inherit (source) url hash; };
           dontUnpack = true;
+          nativeBuildInputs = pkgs.lib.optionals pkgs.stdenv.isLinux [
+            pkgs.autoPatchelfHook
+          ];
+          buildInputs = pkgs.lib.optionals pkgs.stdenv.isLinux [
+            pkgs.glib
+            pkgs.libsecret
+          ];
           installPhase = ''
             mkdir -p $out/bin
             tar -xzf $src -C $out/bin

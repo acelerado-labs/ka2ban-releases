ad33eea7047576034f2159b1562eb6158bb17e87
Stein Martin Hustad <stein@hustad.com>
2026-02-21 05:52:18 +0000

    chore: update flake.nix for v0.5.3

--- flake.nix
+++ flake.nix
@@ -5,20 +5,20 @@
 
   outputs = { self, nixpkgs }:
     let
-      version = "0.5.2";
+      version = "0.5.3";
 
       sources = {
         "aarch64-darwin" = {
-          url = "https://github.com/acelerado-labs/ka2ban-releases/releases/download/v0.5.2/ka2ban-v0.5.2-darwin-arm64.tar.gz";
-          hash = "__DARWIN_ARM64_HASH__";
+          url = "https://github.com/acelerado-labs/ka2ban-releases/releases/download/v0.5.3/ka2ban-v0.5.3-darwin-arm64.tar.gz";
+          hash = "sha256-77b3EV1ALp787lwTp3NdaFSdh7EFC+8OQXpI1xVpYbc=";
         };
         "x86_64-linux" = {
-          url = "https://github.com/acelerado-labs/ka2ban-releases/releases/download/v0.5.2/ka2ban-v0.5.2-linux-x64.tar.gz";
-          hash = "__LINUX_X64_HASH__";
+          url = "https://github.com/acelerado-labs/ka2ban-releases/releases/download/v0.5.3/ka2ban-v0.5.3-linux-x64.tar.gz";
+          hash = "sha256-tiDIgC3nBHW/NBW8Amj6Om7NZZYG8wDatVr1oXlLdYI=";
         };
         "aarch64-linux" = {
-          url = "https://github.com/acelerado-labs/ka2ban-releases/releases/download/v0.5.2/ka2ban-v0.5.2-linux-arm64.tar.gz";
-          hash = "__LINUX_ARM64_HASH__";
+          url = "https://github.com/acelerado-labs/ka2ban-releases/releases/download/v0.5.3/ka2ban-v0.5.3-linux-arm64.tar.gz";
+          hash = "sha256-exeEMZGLz6suv2b694xN2N2AOSizMdg4TTt4v84LWJg=";
         };
       };
 
